c50f541e7c627213d74987865b9143136b3b21a3
KirillShip <shkirkala@gmail.com>
2024-11-28 03:32:26 +0500

    Another update of DB and added student login, main.html

--- login.php
+++ login.php
@@ -1,36 +1,49 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
+$login = "root";
 $password = "root";
 $dbname = "studjobs";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $login, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Ошибка подключения: " . $conn->connect_error);
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
+    $login = $_POST['login'];
     $password = $_POST['password'];
 
-    $username = $conn->real_escape_string($username);
+    $login = $conn->real_escape_string($login);
     $password = $conn->real_escape_string($password);
 
     $hashed_password = md5($password);
 
-    $sql = "SELECT id FROM Admins WHERE username='$username' AND password='$hashed_password'";
+    $sql = "SELECT id FROM Admins WHERE login='$login' AND password='$hashed_password'";
     $id = $conn->query($sql);
 
     if ($id->num_rows > 0) {
         session_start();
         $_SESSION['user'] = $id;
-        header("Location: http://dolg/main.php");
+        $_SESSION['role'] = 'admin';
+        header("Location: http://dolg/main.html");
         exit();
-    } else {
-        echo "<div style='text-align: center; color: red;'>Неверный логин или пароль</div>";
     }
+
+    $sql = "SELECT id FROM Students WHERE login='$login' AND password='$hashed_password'";
+    $id = $conn->query($sql);
+
+    if ($id->num_rows > 0) {
+        session_start();
+        $_SESSION['user'] = $id;
+        $_SESSION['role'] = 'student';
+        header("Location: http://dolg/main.html");
+        exit();
+    }
+
+    echo "<div style='text-align: center; color: red;'>Неверный логин или пароль</div>";
+    
 }
 
 $conn->close();
